d225bba48917ad422310e9dae53151373f8d3c10
matthew cullen <cullenwmatthew@gmail.com>
2020-05-12 13:12:51 -0700

    completed compiling-monster-arrays

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 var base = 1 ;
-var totalXP= null;
+var totalXp= null;
 var pcCount = document.getElementById('pcCount')
 var averageLvl = document.getElementById('averageLvl');
 var difficulty=document.getElementById('difficulty');
 var submitButton = document.getElementById('submitButton');
-
+var challengeRatings = {
+  '0': 10,
+  '1/8': 25,
+  '1/4': 50,
+  '1/2': 100,
+  '1': 200,
+  '2': 450,
+  '3': 700,
+  '4': 1100,
+  '5': 1800,
+  '6': 2300,
+  '7': 2900,
+  '8': 3900,
+  '9': 5000,
+  '10': 5900,
+  '11': 7200,
+  '12': 8400,
+  '13': 10000,
+  '14': 11500,
+  '15': 13000,
+  '16': 15000,
+  '17': 18000,
+  '18': 20000,
+  '19': 22000,
+  '20': 25000,
+  '21': 33000,
+  '22': 41000,
+  '23': 50000,
+  '24': 62000,
+  '30': 155000,
+}
 
 submitButton.addEventListener('click',handleSubmitClick);
 
@@ -12,22 +42,29 @@ submitButton.addEventListener('click',handleSubmitClick);
 
 function handleSubmitClick(e){
   e.preventDefault();
-// average level X number of PCs X difficulty level
-  switch(averageLvl.value){
+  getTotalXp();
+  possiblemonsters(totalXp, 8)
+
+}
+
+
+function getTotalXp(){
+  // average level X number of PCs X difficulty level
+  switch (averageLvl.value) {
     case "1":
-      base=25
+      base = 25
       break;
     case "2":
-      base=50
+      base = 50
       break;
     case "3":
-      base=75
+      base = 75
       break;
     case "4":
-      base=125
+      base = 125
       break;
     case "5":
-      base=250
+      base = 250
       break;
     case "6":
       base = 300
@@ -73,9 +110,67 @@ function handleSubmitClick(e){
       break;
     case "20":
       base = 2800
+  }
+  totalXp = (base * pcCount.value * difficulty.value)
+  console.log(totalXp);
+}
+function possiblemonsters(totalXp, maxMonsters) {
+  var maxChallengeRatings = [];
+  var monstersXp = []
+  var monstersCR = []
+  var orignalTotalXp= totalXp;
+  for(var key in challengeRatings) {
+    if(challengeRatings[key] < totalXp) {
+      maxChallengeRatings.push(key)
+    }
+  }
+  shuffle(maxChallengeRatings);
+  for(var i =0; i<maxChallengeRatings.length;i++){
+    var value = challengeRatings[maxChallengeRatings[i]];
+    if(value<=totalXp){
+      totalXp -= value;
+      monstersXp.push(value);
+      maxMonsters--;
+      if(maxMonsters===0&&totalXp>200){
+        var lowestXp=0;
+        for (var a=0;a<monstersXp.length;a++)
+      } else
+    }
+  }
+  for(var int =0; int <monstersXp.length;int++){
+    for(var prop in challengeRatings){
+      if (monstersXp[int]===challengeRatings[prop]){
+        monstersCR.push(prop)
+      }
+
+    }
+  }
+  console.log('maxChallengeRating:',maxChallengeRatings,'monstersXp:' , monstersXp,'monstersCR', monstersCR);
+  // randomize the array of max challenge ratings
+  // loop through max challenge ratings
+  // find value of that key
+  // subtract that value from the totalxp
+  // once encounter is found create an object with that key and value
+  // push that object into monsters
+  // decrement maxmonsters
+  // if maxmonsters = 0 end loop
+  // if total xp does not have enough to grab another challenge end loop
+  // return possible monsters
+}
+
+function shuffle(array){
+  var currentIndex = array.length
+  var tempValue = null;
+  var randomIndex =null;
+  while (0 !== currentIndex){
+
+    randomIndex= Math.floor(Math.random()* currentIndex);
+    currentIndex -= 1;
+
+    tempValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = tempValue;
 
   }
-  totalXP=(base*pcCount.value*difficulty.value)
-  console.log(totalXP);
-
+  return array;
 }
